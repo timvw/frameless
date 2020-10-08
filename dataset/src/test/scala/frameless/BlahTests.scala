d6ec02b8f46a593984a35470623ba9862b74d9d1
Tim Van Wassenhove <tim@timvw.be>
2020-10-08 21:45:05 +0200

    wip

--- dataset/src/test/scala/frameless/BlahTests.scala
+++ dataset/src/test/scala/frameless/BlahTests.scala
@@ -15,7 +15,8 @@ class BlahTests extends AnyFunSuite with BeforeAndAfterAll with SparkTesting {
 
     val ds: Dataset[Event] = session.createDataset(Seq.empty[Event])
     val tds: TypedDataset[Event] = TypedDataset.create(ds)
-    //val nameColumn: TypedColumn[Event, String] = tds.col(_.name)
+    
+    val nameColumn: TypedColumn[Event, String] = tds.col(_.name)
     val idColumn = tds.col(_.id)
 
     println(idColumn.expr)
